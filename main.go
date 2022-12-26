9035e4207ddb9d34291d3a563a4ac35a374f1e4e
Mike Blum <me+github@mblum.me>
2022-12-26 13:00:55 -0600

    .gitignore + example main.go

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/mikeblum/golang-project-template/conf"
+	"github.com/mikeblum/golang-project-template/log"
+)
+
+func main() {
+	log := log.NewLog()
+	config, err := conf.NewConfig()
+	if err != nil {
+		log.Fatalf("error loading conf: %v", err)
+	}
+	for _, key := range config.AllKeys() {
+		log.Infof("%s", key)
+	}
+}
