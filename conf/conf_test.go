da9d71efda50b7f8fe6f691036d8d5f4eb558cf0
Mike Blum <me+github@mblum.me>
2022-12-26 19:54:45 -0600

    golangci-lent + conf tests

--- conf/conf_test.go
+++ conf/conf_test.go
@@ -1,6 +1,7 @@
 package conf
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,4 +9,14 @@ import (
 
 func TestSomething(t *testing.T) {
 	assert.True(t, true, "True is true!")
+}
+
+func TestGetEnvVar(t *testing.T) {
+	envShell := "SHELL"
+	assert.True(t, len(strings.TrimSpace(GetEnv(envShell, ""))) > 0)
+}
+
+func TestGetEnvDefault(t *testing.T) {
+	expected := "default env"
+	assert.Equal(t, expected, strings.TrimSpace(GetEnv("", expected)))
 }
